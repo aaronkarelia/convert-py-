f25fa1f1c329bad14fdbf74c4c07d9704a501671
aaronkarelia <aaron@gibsonandlily.com>
2015-01-30 14:38:37 -0700

    Update convert.py
    Now includes a listing from 0 to100 celsius.

--- convert.py
+++ convert.py
@@ -4,8 +4,9 @@
 def main():
     print("The program will convert celsius to fahrenheit.")    
     celsius = eval(input("What is the Celsius temperature? "))
-    for celsius in range(5):
+    for celsius in [0, 10, 20, 30, 40, 50, 60, 70 ,80, 90, 100]:
         fahrenheit = 9/5 * celsius + 32
-        print("The temperature is", fahrenheit, "degrees Fahrenheit.")
+        print('The temperature is', celsius, 'celsius and', fahrenheit, 'degrees Fahrenheit.')
 
 main()
+
